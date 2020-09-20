47c0f522e35d80afb009bff431265860450855c1
Jack B. Muir <jmuir@caltech.edu>
2020-09-20 11:41:53 -0700

    franklin-update

--- publish_to_gps.jl
+++ publish_to_gps.jl
@@ -2,11 +2,8 @@ using Franklin
 using Glob
 
 function push_to_gps()
-    files = glob("__site/*")
-    for file in files
-        cmd = `rsync -av $file jmuir@newton.gps.caltech.edu:/home/GPSUserWebPages/jmuir`
-        run(cmd)
-    end
+    cmd = `rsync -av __site/ jmuir@newton.gps.caltech.edu:/home/GPSUserWebPages/jmuir`
+    run(cmd)
     return nothing
 end
 

c55fb4339f1def6ab4dab5a8402e0d5c772c57f4
Jack B. Muir <jmuir@caltech.edu>
2020-10-08 10:28:08 -0700

    franklin-update

--- utils.jl
+++ utils.jl
@@ -1,16 +1,5 @@
-function hfun_bar(vname)
-  val = Meta.parse(vname[1])
-  return round(sqrt(val), digits=2)
-end
-
-function hfun_m1fill(vname)
-  var = vname[1]
-  return pagevar("index", var)
-end
-
-function lx_baz(com, _)
-  # keep this first line
-  brace_content = Franklin.content(com.braces[1]) # input string
-  # do whatever you want here
-  return uppercase(brace_content)
+function lx_reversed(com, _)
+  brace_content = Franklin.content(com.braces[1])
+  html = Franklin.fd2html(brace_content, internal=true)
+  return "~~~" * replace(html, "<ol>" => "<ol reversed>") * "~~~"
 end
